3de6fdc07e22396f29b1a2dfc779d95ec4c28094
anhgelus <anhgelus.morhtuuzh@proton.me>
2024-10-29 16:04:38 +0000

    feat(time): day played in multiplayer

---
+++ src/client/java/world/anhgelus/architectsland/daycounterenhanced/client/ClientStorage.java
@@ -0,0 +1,56 @@
+package world.anhgelus.architectsland.daycounterenhanced.client;
+
+import net.fabricmc.loader.api.FabricLoader;
+import world.anhgelus.architectsland.daycounterenhanced.DayCounterEnhanced;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ClientStorage {
+    private static Map<String, Long> map = null;
+
+    public static long get(String key) {
+        if (map == null) initMap();
+        assert map != null;
+        return map.getOrDefault(key, 0L);
+    }
+
+    public static void set(String key, long value) {
+        if (map == null) initMap();
+        map.put(key, value);
+    }
+
+    private static void initMap() {
+        map = new HashMap<>();
+        final var file = path().toFile();
+        Scanner scanner;
+        try {
+            if (file.createNewFile()) return;
+            scanner = new Scanner(file);
+        } catch (IOException e) {
+            DayCounterEnhanced.LOGGER.error(e.toString());
+            return;
+        }
+        while (scanner.hasNextLine()) {
+            final var splitted = scanner.nextLine().split("=");
+            map.put(splitted[0].trim(), Long.parseLong(splitted[1].trim()));
+        }
+    }
+
+    public static void writeMap() throws IOException {
+        if (map == null) return;
+        try (final var writer = new FileWriter(path().toString())) {
+            for (var k : map.keySet()) {
+                writer.write(k+"="+map.get(k));
+            }
+        }
+    }
+
+    private static Path path() {
+        return FabricLoader.getInstance().getGameDir().resolve("daycounterenhanced.properties");
+    }
+}
--- src/client/java/world/anhgelus/architectsland/daycounterenhanced/client/DayCounterEnhancedClient.java
+++ src/client/java/world/anhgelus/architectsland/daycounterenhanced/client/DayCounterEnhancedClient.java
@@ -2,37 +2,59 @@ package world.anhgelus.architectsland.daycounterenhanced.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.stat.Stats;
 import world.anhgelus.architectsland.daycounterenhanced.DayCounterEnhanced;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DayCounterEnhancedClient implements ClientModInitializer {
+    private long connectedAt = -1;
+    private String address = "";
+    private long lastTimeConnected = 0;
 
     @Override
     public void onInitializeClient() {
-        AtomicInteger i = new AtomicInteger();
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
             final var client = MinecraftClient.getInstance();
             final var player = client.player;
             final var world = client.world;
             if (world == null || player == null) return;
-            i.getAndIncrement();
             if (client.isIntegratedServerRunning()) {
-                if (i.get() % 140 == 0) DayCounterEnhanced.LOGGER.info("{}", world.getTime());
                 drawContext.drawTextWithShadow(client.textRenderer, "Day " + (Math.floorDiv(world.getTime(),20000)+1), 5, 5, 0xFFFFFF);
                 return;
             }
-            // not working
-            final var time = player.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME));
-            if (i.get() % 140 == 0) DayCounterEnhanced.LOGGER.info("{}", time);
-            drawContext.drawTextWithShadow(client.textRenderer, "Day " + (Math.floorDiv(time,200000)+1), 5, 5, 0xFFFFFF);
+            drawContext.drawTextWithShadow(client.textRenderer, "Day " + (Math.floorDiv(timeConnected(),20*60*10)+1), 5, 5, 0xFFFFFF);
         });
-//        ClientLifecycleEvents.CLIENT_STARTED.register(t -> {
-//
-//        });
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            if (client.isIntegratedServerRunning()) return;
+            address = handler.getConnection().getAddressAsString(true);
+            connectedAt = System.currentTimeMillis();
+            lastTimeConnected = ClientStorage.get(address);
+        });
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
+            if (client.isIntegratedServerRunning()) return;
+            if (connectedAt == -1) throw new IllegalStateException("Connected at was not set");
+            ClientStorage.set(address, timeConnected());
+            // reset
+            address = "";
+            connectedAt = -1;
+            lastTimeConnected = 0;
+        });
+
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> {
+            try {
+                ClientStorage.writeMap();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private long timeConnected() {
+        return Math.floorDiv(System.currentTimeMillis() - connectedAt, 100) + lastTimeConnected;
     }
 }
