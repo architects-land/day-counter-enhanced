5f21ca63919293c52a687f43b2955f722e674507
anhgelus <anhgelus.morhtuuzh@proton.me>
2025-05-31 13:25:48 +0000

    feat(server): use server's playtime's base on connection

--- src/client/java/world/anhgelus/architectsland/daycounterenhanced/client/DayCounterEnhancedClient.java
+++ src/client/java/world/anhgelus/architectsland/daycounterenhanced/client/DayCounterEnhancedClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.network.packet.c2s.play.ClientStatusC2SPacket;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
 import world.anhgelus.architectsland.daycounterenhanced.DayCounterEnhanced;
@@ -17,6 +18,7 @@ public class DayCounterEnhancedClient implements ClientModInitializer {
     private long connectedAt = -1;
     private String address = "";
     private long lastTimeConnected = 0;
+    private boolean firstUpdateDone = false;
 
     public final Identifier HUD_ID = Identifier.of(DayCounterEnhanced.MOD_ID, "hud");;
 
@@ -33,8 +35,20 @@ public class DayCounterEnhancedClient implements ClientModInitializer {
                         final var player = client.player;
                         final var world = client.world;
                         if (world == null || player == null) return;
+                        final var playTimeStat = Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME);
                         if (!client.isIntegratedServerRunning()) {
-                            draw(client, context, Math.floorDiv(timeConnected(),20*60*10)+1);
+                            if (!firstUpdateDone) {
+                                final var packet = new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.REQUEST_STATS);
+                                final var network = client.getNetworkHandler();
+                                if (network == null) {
+                                    DayCounterEnhanced.LOGGER.warn("Network handler is null");
+                                    return;
+                                }
+                                network.sendPacket(packet);
+                                lastTimeConnected = player.getStatHandler().getStat(playTimeStat);
+                                firstUpdateDone = true;
+                            }
+                            draw(client, context, Math.floorDiv(timeConnected(),20*60*20)+1);
                             return;
                         }
                         // get server player entity
@@ -44,6 +58,7 @@ public class DayCounterEnhancedClient implements ClientModInitializer {
                         assert serverPlayer != null;
                         // get and show time
                         final var time = serverPlayer.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME));
+                        serverPlayer.getStatHandler().updateStatSet();
                         draw(client, context, Math.floorDiv(time,24000)+1);
                     }
             );
@@ -54,6 +69,7 @@ public class DayCounterEnhancedClient implements ClientModInitializer {
             address = handler.getConnection().getAddressAsString(true);
             connectedAt = System.currentTimeMillis();
             lastTimeConnected = ClientStorage.get(address);
+            firstUpdateDone = false;
         });
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
             if (client.isIntegratedServerRunning()) return;
